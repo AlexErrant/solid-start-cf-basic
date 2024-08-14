58879d1ad1e00c108300f7a7f962a32e24076010
Alex <git@pentive.com>
2024-08-14 13:41:32 -0500

    add ClientCounter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,25 @@
 import { Title } from "@solidjs/meta";
 import Counter from "~/components/Counter";
+import { clientOnly } from '@solidjs/start'
+
+const ClientCounter = clientOnly(
+	async () => await import('~/components/Counter'),
+)
+
 
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
-      <Counter />
+      <div>
+         Plain Counter:
+        <Counter />
+      </div>
+      <div>
+        Client Counter:
+        <ClientCounter />
+      </div>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
